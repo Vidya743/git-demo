60c5164f97e6d8b7fd20ef82ba47f6baa8efb863
vidya <vidyashreepvidya@gmail.com>
2025-02-15 03:29:35 -0800

    adding product entity

---
+++ src/main/java/com/example/git/demo/product.java
@@ -0,0 +1,42 @@
+package com.example.git.demo;
+
+public class product {
+    int id;
+    String name;
+    int price;
+
+    public int getQuantiy() {
+        return quantiy;
+    }
+
+    public void setQuantiy(int quantiy) {
+        this.quantiy = quantiy;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    int quantiy;
+
+}
